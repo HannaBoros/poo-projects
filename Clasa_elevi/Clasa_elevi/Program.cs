eb5b4c1a217ee10645ba2c9551cf4aa675cd6eaa
Hanna <hannaboros19@gmail.com>
2020-04-28 17:18:24 +0300

    Added new project

---
+++ Clasa_elevi/Clasa_elevi/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clasa_elevi
+{
+    class Program
+    {
+        static char[] delimiterChars = { ' '};
+        public static Student lineToPerson(string line)
+        {
+            string[] delimitedLines = line.Split(delimiterChars);
+            Student s = new Student();
+            s.lastName = delimitedLines[0];
+            s.firstName = delimitedLines[1];
+            int note = int.Parse(delimitedLines[2]);
+           double sum = 0;
+ 
+               
+                for (int i=3; i < delimitedLines.Length; i++)
+                {
+                    sum = sum + int.Parse(delimitedLines[i]);
+                }
+            s.average = sum / note;
+           
+            return s;
+     
+        }
+        public static void DisplayStudents(List<Student> students)
+        {
+            foreach(Student s in students)
+            {
+                Console.WriteLine(s.ToString());
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+           // Student s = new Student();
+            // read the file lines
+            List<string> lines = FileIO.file_Reader("../../input.txt");
+            Console.WriteLine("Print the lines");
+            foreach(string v in lines)
+            {
+                Console.WriteLine(v);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Create the students");
+            List<Student> students = new List<Student>();
+            foreach (string line in lines)
+            {
+                Student s= lineToPerson(line);
+                students.Add(s);
+            }
+            Console.WriteLine("Print the students");
+            DisplayStudents(students);
+
+            Console.WriteLine();
+            Console.WriteLine("Order students by avarage grade");
+            students.Sort();
+            DisplayStudents(students);
+
+
+
+        }
+    }
+}
---
+++ Clasa_elevi/Clasa_elevi/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Clasa_elevi
+{
+    class Student : IEquatable<Student>, IComparable<Student>
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public double average { get; set; }
+        public Student()
+        {
+
+        }
+
+        public override string ToString()
+        {
+
+            return lastName + ' ' + firstName + "=>"+ " Average:" + ' '+ average;
+
+        }
+
+        public bool Equals(Student other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(Student s)
+        {
+            if (this.average < s.average) return 1;
+            if (this.average > s.average) return -1;
+
+            StringComparer comparer = StringComparer.Create(new CultureInfo("ro-RO"), true);
+
+            int orderLastName = comparer.Compare(lastName, s.lastName);
+            if (orderLastName != 0)
+                return orderLastName;
+
+         
+            int orderFirstName = comparer.Compare(firstName, s.firstName);
+            if (orderFirstName != 0)
+                return orderFirstName;
+            
+        
+            return 0;
+        }
+    }
+}
