a471f1d6c5ee16984e2dc18437edab6b97b4ad8e
Hanna <hannaboros19@gmail.com>
2020-05-13 20:50:56 +0300

    Added Stack Project

---
+++ Stack/Stiva/Stiva/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stiva
+{
+
+   
+
+    class Program
+    {
+        public static void test_push_pop()
+        {
+            GenericStack<int> stackOfInts = new GenericStack<int>(4);
+            stackOfInts.Push(1);
+            stackOfInts.Push(2);
+            stackOfInts.Push(3);
+            Console.WriteLine("Stack:");
+            Console.WriteLine(stackOfInts.ToString());
+            int nrPop = stackOfInts.Pop();
+            Console.WriteLine("Popped Stack");
+            Console.WriteLine(stackOfInts.ToString());
+            Console.WriteLine($"popped number: {nrPop}");
+        }
+
+        public static void test_push_exception()
+        {
+            GenericStack<int> stackOfInts = new GenericStack<int>(1);
+            stackOfInts.Push(1);
+            Console.WriteLine(stackOfInts.ToString());
+
+            stackOfInts.Push(2);
+            Console.WriteLine(stackOfInts.ToString());
+
+        }
+
+        public static void test_pop_exception()
+        {
+            GenericStack<int> stackOfInts = new GenericStack<int>(2);
+            int nr=  stackOfInts.Pop();
+            Console.WriteLine(stackOfInts.ToString());
+        }
+
+        public static void test_clear()
+        {
+            GenericStack<string> stackOfStrings = new GenericStack<string>(4);
+            stackOfStrings.Push("licenta");
+            stackOfStrings.Push("master");
+            stackOfStrings.Push("doctorat");
+            Console.WriteLine("Stack of strings");
+            Console.WriteLine(stackOfStrings.ToString());
+            stackOfStrings.Clear();
+            Console.WriteLine("Cleared Stack");
+            Console.WriteLine(stackOfStrings.ToString());
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            test_push_pop();
+            Console.WriteLine();
+            //Console.WriteLine("Testing push argument exeption");
+            //test_push_exception();
+            // Console.WriteLine("Testing push argument exeption");
+            // test_pop_exception();
+            test_clear();
+
+
+        }
+    }
+}
---
+++ Stack/Stiva/Stiva/Stack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Stiva
+{
+    public class GenericStack<T>
+    {
+        public List<T> stack ;
+        public int size { get; }
+        public GenericStack(int size)
+        {
+            this.stack = new List<T>();
+            this.size = size;
+        }
+
+        private int top()
+        {
+            return this.stack.Count() - 1;
+        }
+        public void Push(T elem)
+        {
+            if (top()<this.size-1)
+            {
+                stack.Add(elem);
+            }
+            else
+            {
+                throw new ArgumentException("Stack is Full");
+            }
+        }
+        private bool IsEmpty()
+        {
+            return top()+1 == 0;
+        }
+        public T Pop()
+        {
+           if(!IsEmpty())
+           {
+                T t = stack.ElementAt(top());
+                stack.RemoveAt(top());
+                return t;
+           }
+           else
+           {
+                throw new ArgumentException("Stack is Empty");
+           }
+        }
+        public void Clear()
+        {
+            stack.Clear();
+        }
+
+        
+        public override string ToString()
+        {
+           
+            StringBuilder sb = new StringBuilder("");
+            for (int i = stack.Count()-1; i >=0; i--)
+            {
+                sb.Append(stack.ElementAt(i) + "\n");
+            }
+           return sb.ToString();
+            
+        }
+    }
+
+}
