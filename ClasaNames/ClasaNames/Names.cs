088b87035ff2be06c47988becabfd14e24a5b5b9
Hanna <hannaboros19@gmail.com>
2020-05-29 17:07:34 +0300

    Added ClasaNames Project

---
+++ ClasaNames/ClasaNames/Names.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasaNames
+{
+    public class Names
+    {
+
+        public List<string> names;
+        public int dimensiune;
+        public bool error;
+        public Names(int dimensiune)
+        {
+            this.names = new List<string>();
+
+            this.dimensiune = dimensiune;
+            for (int i = 0; i < dimensiune; i++)
+            {
+                this.names.Insert(i, "empty");
+            }
+        }
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < dimensiune)
+                {
+                    error = false;
+                    return names.ElementAt(index);
+                }
+                else
+                {
+                    error = true;
+                    throw new ArgumentException("Index out of range");
+
+                    
+                }
+            }
+            set
+            {
+                if (index >= 0 && index < dimensiune)
+                {
+                    names.Insert(index, value);
+                    error = false;
+                }
+                else
+                {
+                    error = true;
+                    throw new ArgumentException("Index out of range");
+
+                }
+            }
+        }
+        public string this[string indexer]
+        {
+            get
+            {
+                int index;
+                try
+                { index = int.Parse(indexer); }
+                catch (FormatException)
+                {
+                    throw new ArgumentException("string index not valid integer");
+                }
+                if (index >= 0 && index < dimensiune)
+                {
+                    error = false;
+                    return names.ElementAt(index);
+                }
+                else
+                {
+                    error = true;
+                    throw new ArgumentException("Index out of range");
+
+                   
+                }
+            }
+
+        }
+        public override string ToString()
+        {
+            string r = "";
+           for(int i = 0; i<this.dimensiune;i++)
+           {
+                r += i+ ":" + this[i]+"\n";
+           }
+            return r;
+
+        }
+
+
+
+
+    }
+}
---
+++ ClasaNames/ClasaNames/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasaNames
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Names a = new Names(4);
+            a[0] = "Hanna";
+            a[1] = "Boros";
+            Console.WriteLine(a.ToString());
+
+           //testing index out of range exeption
+           //   a[5] = "error";
+          
+            //testing indexer of type string
+            Console.WriteLine(a["1"]);
+
+            //testing not corract format of indexer
+            //Console.WriteLine(a["one"]);
+
+
+
+        }
+    }
+}
